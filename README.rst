2508a0f3c8687d47cc7f3cfc7748eadb529ea5b6
wujingcheng <21619014@zju.edu.cn>
2018-08-19 15:49:34 +0800

    Add files via upload

---
+++ README.rst
@@ -0,0 +1,24 @@
+Download the latest version of DeepHLApan from 
+
+## Installation
+
+Unzip the source code and go into the directory by using the following command:
+
+tar xvzf deephlapan-*.tar.gz
+
+cd deephlapan
+
+Invoke the setup script:
+
+python setup.py install
+
+## Input files
+
+DeepHLApan takes csv files as input with head of "Annotation,HLA,peptide".
+
+## General usage
+
+trust -F YOUR_file -O YOUR_output_path
+
+
+
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+'''
+DeepHLApan set up script
+'''
+
+import os, sys, re
+from setuptools import setup, Extension
+from distutils.sysconfig import get_python_inc
+
+NAME = 'deephlapan'
+PACKAGE = [NAME]
+VERSION = __import__(NAME).__version__
+try:
+    f = open("requirements.txt", "rb")
+    REQUIRES = [i.strip() for i in f.read().decode("utf-8").split("\n")]
+except:
+    print("'requirements.txt' not found!")
+    REQUIRES = list()
+
+incdir = get_python_inc(plat_specific=1)
+
+def path_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            if filename[0] is not '.':  # filter hidden files
+                paths.append(os.path.join(re.sub(NAME+'/', '', path), filename))
+    return paths
+
+model = path_files(NAME)
+
+def main():
+    setup(name=NAME,
+        version=VERSION,
+        description='Used for predicting the binding level between peptides and HLA alleles',
+        long_description=open('README.rst').read(),
+        author='Jingcheng Wu',
+        author_email='21619014@zju.edu.cn',
+        url='https://github.com/',
+        packages=PACKAGE,
+        package_dir={NAME: NAME},
+        package_data={NAME: model},
+        scripts=['bin/deephlapan'],
+        install_requires=REQUIRES,
+        license=open('LICENSE').read()
+        )
+
+
+if __name__ == '__main__':
+    main()
+
